690b31f2ab3ffab62aa0c090a296b242359e1f2d
Manav Kumar <manavkumar@Manavs-MacBook-Pro.local>
2026-05-23 18:43:27 +0530

    bc

--- application.py
+++ application.py
@@ -32,4 +32,4 @@ def predict_datapoint():
         results=predict_pipeline.predict(features=pred_df)
         return render_template('home.html',results=results[0])
 if __name__=="__main__":
-    app.run(host='0.0.0.0',debug=True,port=5001)
+    app.run(host='0.0.0.0')
